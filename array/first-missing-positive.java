8bb8877ca8d48c851421d691715365f2395aa919
Zhengyuan <sli695439@gmail.com>
2026-01-29 16:03:41 -0600

    LC-first-missing-positive [Runtime: 1 ms; Memory: 71.4 MB]

--- array/first-missing-positive.java
+++ array/first-missing-positive.java
@@ -1,5 +1,6 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
+        //cannot use sort
         //iterate the nums
         //starting from 1, find the smallest positive
         //if found increase 1
@@ -7,10 +8,23 @@ class Solution {
         int smallest = 1;
         int i = 0;
         while (i < nums.length) {
-            if (nums[i] == smallest) {
+            //if nums[i] <= n
+            //swap at the correct location
+            //update the smallest
+            int correct = nums[i] - 1;
+            if (correct < nums.length && correct >= 0 && nums[i] != nums[correct]) {
+                int temp = nums[i];
+                nums[i] = nums[correct];
+                nums[correct] = temp;
+            }else {
+                i++;
+            }
+        }
+        //two times
+        for (int j = 0; j<nums.length; j++) {
+            if (nums[j] == smallest) {
                 smallest += 1;
             }
-            i++;
         }
         return smallest;
     }
