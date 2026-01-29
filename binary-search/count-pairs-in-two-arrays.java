7e65d962c9bf3f4528a73fbea502c4b6a5c0d2e5
Zhengyuan <sli695439@gmail.com>
2026-01-29 17:13:29 -0600

    LC-count-pairs-in-two-arrays [Runtime: 42 ms; Memory: 83.6 MB]

--- binary-search/count-pairs-in-two-arrays.java
+++ binary-search/count-pairs-in-two-arrays.java
@@ -3,16 +3,31 @@ class Solution {
         //change the order with nums1[i] - nums2[i] + (nums1[j] - nums2[j])> 0
         //sanity check
         if (nums1.length != nums2.length) return -1;
+
         long count = 0;
         int[] s1 = new int[nums1.length];
         for (int i = 0; i<nums1.length; i++) {
             s1[i] = nums1[i] - nums2[i];
         }
-        for (int i = 0; i<s1.length - 1; i++) {
-            for (int j = i+1; j<s1.length; j++) {
-                if (s1[i] + s1[j] > 0) {
-                    count++;
+        Arrays.sort(s1);
+        
+        for (int i = 0; i< s1.length; i++) {
+            if (s1[i] > 0) {
+                count += s1.length - i - 1;
+            }else {
+                int left = i+1;
+                int right = s1.length - 1;
+                //find the first one that is valid
+                while (left <= right) {
+                    int mid = left + (right - left) / 2;
+
+                    if (s1[mid] + s1[i] > 0) {
+                        right = mid - 1;
+                    }else {
+                        left = mid + 1;
+                    }
                 }
+                count += s1.length - left;
             }
         }
         return count;
