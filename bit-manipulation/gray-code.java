b61d54d41b871ade23d7c0d7919bc192fcec39e2
Zhengyuan <sli695439@gmail.com>
2026-02-03 20:00:56 -0500

    LC-gray-code [Runtime: 7 ms; Memory: 53.8 MB]

---
+++ bit-manipulation/gray-code.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public List<Integer> grayCode(int n) {
+        List<Integer> result = new ArrayList<>();
+        result.add(0);
+
+        for (int i = 1; i <= n; i++) {
+            int previousSequenceLength = result.size();
+            int mask = 1 << (i - 1);
+            for (int j = previousSequenceLength - 1; j >= 0; j--) {
+                result.add(mask + result.get(j));
+            }
+        }
+        return result;
+    }
+}
