df1a1d09795f585452028f47b07780490d5d54a9
Zhengyuan <sli695439@gmail.com>
2026-02-12 21:27:28 -0500

    LC-letter-tile-possibilities [Runtime: 1 ms; Memory: 42.9 MB]

--- backtracking/letter-tile-possibilities.java
+++ backtracking/letter-tile-possibilities.java
@@ -1,25 +1,27 @@
 class Solution {
     public int numTilePossibilities(String tiles) {
         char[] chars = tiles.toCharArray();
-        Arrays.sort(chars); // Sort to handle duplicates
+        Arrays.sort(chars);
         boolean[] used = new boolean[chars.length];
         return dfs(chars, used);
     }
 
-    private int dfs(char[] chars, boolean[] used) {
+    public int dfs(char[] chars, boolean[] used) {
         int count = 0;
 
-        for (int i = 0; i < chars.length; i++) {
+        for (int i = 0; i<chars.length; i++) {
+            //if starting with used[i] used before, skip
             if (used[i]) continue;
-
-            // Skip duplicates: if previous same char is not used, skip
+            //if i >0 and same as i - 1, and [i - 1] is used before, skip
+            //used mean starting with that char
             if (i > 0 && chars[i] == chars[i - 1] && !used[i - 1]) continue;
-
             used[i] = true;
-            count += 1 + dfs(chars, used); // +1 for current string, +dfs for longer ones
-            used[i] = false;
-        }
 
+            count += 1 + dfs(chars, used);
+            //undo
+            used[i] = false;
+
+        }
         return count;
     }
 }
