ae905df762f99b733cf4cd00bf309ea4357338c8
Zhengyuan <sli695439@gmail.com>
2026-02-21 12:30:48 -0500

    LC-number-of-provinces [Runtime: 1 ms; Memory: 47.3 MB]

--- breadth-first-search/number-of-provinces.java
+++ breadth-first-search/number-of-provinces.java
@@ -1,22 +1,51 @@
+//use unionfind
+class UnionFind{
+    int[] parent;
+    int[] rank;
+
+    public UnionFind(int n){
+        parent = new int[n];
+        for (int i = 0; i<n; i++) {
+            parent[i] = i;
+        }
+        rank = new int[n];
+    }
+
+    public int find(int num){
+        if (num != parent[num]) {
+            parent[num] = find(parent[num]);
+        }
+        return parent[num];
+    }
+
+    public void union(int i, int j) {
+        int iP = find(i), jP = find(j);
+        if (iP == jP) {
+            return;
+        }else if (rank[iP] > rank[jP]) {
+            parent[jP] = iP;
+        }else if (rank[iP] < rank[jP]){
+            parent[iP] = jP;
+        }else {
+            parent[jP] = iP;
+            rank[iP]++;
+        }
+    }
+}
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        //1 means connected
-        //0 means not connected
         int n = isConnected.length;
-        int result = n;
-        //using map to show how many cities are connected
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i<n; i++) {
-            for (int j = 0; j<n; j++) {
-                if (isConnected[i][j] == 1 && i != j) {
-                    map.put(i, map.getOrDefault(i, 0) + 1);
+        UnionFind uf = new UnionFind(n);
+        int count = n;
+
+        for (int i = 0; i< n; i++) {
+            for (int j = i+1; j<n; j++) {
+                if (isConnected[i][j] == 1 && uf.find(i) != uf.find(j)) {
+                    count--;
+                    uf.union(i, j);
                 }
             }
         }
-        int count = 0;
-        for (int value: map.values()) {
-            count = Math.max(count, value);
-        }
-        return result - count;
+        return count;
     }
 }
