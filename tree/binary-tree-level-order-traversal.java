62f04938ada3abb159a3ee804b35194fa39125c9
Zhengyuan <sli695439@gmail.com>
2026-02-22 12:24:06 -0500

    LC-binary-tree-level-order-traversal [Runtime: 0 ms; Memory: 46.9 MB]

--- tree/binary-tree-level-order-traversal.java
+++ tree/binary-tree-level-order-traversal.java
@@ -14,24 +14,23 @@
  * }
  */
 class Solution {
+    List<List<Integer>> result = new ArrayList<>();
     public List<List<Integer>> levelOrder(TreeNode root) {
-       //a linkedlist to store the val
-       //iterate in the size, add the node val to the result
-       if (root == null) return new ArrayList<>();
-       Deque<TreeNode> dq = new LinkedList<>();
-       dq.add(root);
-       List<List<Integer>> result = new ArrayList<>();
-       while (!dq.isEmpty()) {
-            int size = dq.size();
-            List<Integer> single = new ArrayList<>();
-            for (int i = 0; i<size; i++) {
-                TreeNode current = dq.poll();
-                if (current.left != null) dq.add(current.left);
-                if (current.right != null) dq.add(current.right);
-                single.add(current.val);
-            }
-            result.add(new ArrayList<>(single));
-       }
-       return result;
+        //index the level of the node
+        dfs(root, 0);
+        return result;
+    }
+
+    public void dfs(TreeNode node, int level){
+        //base case
+        if (node == null) return;
+
+        if (result.size() == level) {
+            result.add(new ArrayList<>());
+        }
+
+        result.get(level).add(node.val);
+        dfs(node.left, level + 1);
+        dfs(node.right, level + 1);
     }
 }
