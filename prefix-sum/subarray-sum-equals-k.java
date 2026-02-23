5a2781188b43fd179ee19914f6b669294ef1d18f
Zhengyuan <sli695439@gmail.com>
2026-02-23 17:09:04 -0500

    LC-subarray-sum-equals-k [Runtime: N/A; Memory: N/A]

---
+++ prefix-sum/subarray-sum-equals-k.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        //using the hashmap to record the count of sum
+        Map<Integer, Integer> map = new HashMap<>();
+        //this means sum 0 occurs once
+        map.put(0, 1);
+        int count = 0;
+        int sum = 0;
+
+        for (int num: nums) {
+            sum += num;
+
+            if (map.containsKey(sum - k)) {
+                count += 1;
+            }
+
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return count;
+    }
+}
