229a532248096b94b6bf3b76d72f1d595722a5c6
Zhengyuan <sli695439@gmail.com>
2026-02-23 19:20:28 -0500

    LC-longest-semi-repeating-subarray [Runtime: N/A; Memory: N/A]

--- sliding-window/longest-semi-repeating-subarray.java
+++ sliding-window/longest-semi-repeating-subarray.java
@@ -13,10 +13,13 @@ class Solution {
         int left = 0;
 
         int count = 0;
+
+        Set<Integer> set = new HashSet<>();
         for (int right = 0; right<nums.length; right++) {
             map.put(nums[right], map.getOrDefault(nums[right], 0) + 1);
-            if (map.get(nums[right]) > 1) {
+            if (map.get(nums[right]) > 1 && !set.contains(nums[right])) {
                 count += 1;
+                set.add(nums[right]);
             }
 
             while (count >k) {
@@ -27,6 +30,7 @@ class Solution {
                     if (map.get(nums[left]) == 0) {
                         map.remove(nums[left]);
                     }
+                    set.remove(nums[left]);
                 }
                 left++;
             }
