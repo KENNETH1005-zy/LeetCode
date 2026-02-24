cd1caacd1127361120adc453f4f9f9c6eb83248e
Zhengyuan <sli695439@gmail.com>
2026-02-23 19:31:58 -0500

    LC-longest-semi-repeating-subarray [Runtime: 84 ms; Memory: 131.5 MB]

--- sliding-window/longest-semi-repeating-subarray.java
+++ sliding-window/longest-semi-repeating-subarray.java
@@ -1,13 +1,46 @@
 class Solution {
-    private static final int[] freq = new int[100001]; //reuse this frequency array each time
     public int longestSubarray(int[] nums, int k) {
-        for(int num : nums) freq[num] = 0; //set the relevant values in the frequency array to 0
-        
+        Map<Integer, Integer> map = new HashMap<>();
+        Set<Integer> set = new HashSet<>();
+        int count = 0; // number of distinct values that are duplicated (appear > 1 time)
+        int max = 0;
         int left = 0;
-        for(int num : nums) {
-            if(++freq[num] == 2) k--; //decrease k for each duplicate element we see
-            if(k < 0 && freq[nums[left++]]-- == 2) k++; //if k < 0, increment the left pointer, update freq array and k
+
+        for (int right = 0; right < nums.length; right++) {
+            int num = nums[right];
+            map.put(num, map.getOrDefault(num, 0) + 1);
+
+            // If this is the first time this number becomes duplicated (i.e., goes from 1 → 2)
+            if (map.get(num) == 2 && !set.contains(num)) {
+                count++;
+                set.add(num);
+            }
+
+            // Shrink window while more than k distinct numbers are duplicated
+            while (count > k) {
+                int leftNum = nums[left];
+                int oldCount = map.get(leftNum);
+                int newCount = oldCount - 1;
+
+                map.put(leftNum, newCount);
+
+                // If it drops from 2 → 1, it's no longer duplicated
+                if (oldCount == 2) {
+                    count--;
+                    set.remove(leftNum);
+                }
+
+                // Remove key if count becomes 0
+                if (newCount == 0) {
+                    map.remove(leftNum);
+                }
+
+                left++;
+            }
+
+            max = Math.max(max, right - left + 1);
         }
-        return nums.length - left; //size of the max valid range
+
+        return max;
     }
 }
