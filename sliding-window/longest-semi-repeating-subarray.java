3b874e183cf2697746a189492882e50e78dec670
Zhengyuan <sli695439@gmail.com>
2026-02-23 19:31:44 -0500

    LC-longest-semi-repeating-subarray [Runtime: 5 ms; Memory: 120.3 MB]

--- sliding-window/longest-semi-repeating-subarray.java
+++ sliding-window/longest-semi-repeating-subarray.java
@@ -1,41 +1,13 @@
 class Solution {
+    private static final int[] freq = new int[100001]; //reuse this frequency array each time
     public int longestSubarray(int[] nums, int k) {
-        //map to record the num and its count
-        //if containsKey num, means it is a duplicate, count the repeating num + 1
-        //if the duplicate num is more than k
-        //remove from the left most, if the count of the left is 1, or 0(need to remove from the map), count--
-        //update the max length
-        //return max length
-        Map<Integer, Integer> map = new HashMap<>();
-
-        int max = 0;
-
+        for(int num : nums) freq[num] = 0; //set the relevant values in the frequency array to 0
+        
         int left = 0;
-
-        int count = 0;
-
-        Set<Integer> set = new HashSet<>();
-        for (int right = 0; right<nums.length; right++) {
-            map.put(nums[right], map.getOrDefault(nums[right], 0) + 1);
-            if (map.get(nums[right]) > 1 && !set.contains(nums[right])) {
-                count += 1;
-                set.add(nums[right]);
-            }
-
-            while (count >k) {
-                map.put(nums[left], map.get(nums[left]) - 1);
-
-                if (map.get(nums[left]) <= 1) {
-                    count -= 1;
-                    if (map.get(nums[left]) == 0) {
-                        map.remove(nums[left]);
-                    }
-                    set.remove(nums[left]);
-                }
-                left++;
-            }
-            max = Math.max(max, right - left + 1);
+        for(int num : nums) {
+            if(++freq[num] == 2) k--; //decrease k for each duplicate element we see
+            if(k < 0 && freq[nums[left++]]-- == 2) k++; //if k < 0, increment the left pointer, update freq array and k
         }
-        return max;
+        return nums.length - left; //size of the max valid range
     }
 }
