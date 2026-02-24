58a8f086ff8cfb7cd626eda6b582d333b08d6bf3
Zhengyuan <sli695439@gmail.com>
2026-02-24 13:00:16 -0500

    LC-check-if-array-pairs-are-divisible-by-k [Runtime: N/A; Memory: N/A]

---
+++ hash-table/check-if-array-pairs-are-divisible-by-k.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean canArrange(int[] arr, int k) {
+        //use map to store the remainder and its count
+        //iterate every arr, if the rem is 0 but the count is not even return false
+        //if the rem and k - rem count is not the same return false
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i: arr) {
+            int rem = (i % k + k) % k;
+            map.put(rem, map.getOrDefault(rem, 0) + 1);
+        }
+
+        for (int i: arr) {
+            int rem = (i % k + k) % k;
+
+            if (rem == 0) {
+                if (map.get(rem) % 2 == 1) return false;
+                
+            }else {
+                if (map.get(rem) != map.get(k - rem)) return false;
+            }
+        }
+        return true;
+    }
+}
