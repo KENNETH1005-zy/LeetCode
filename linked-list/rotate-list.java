f76477000fe7f8450a8eb78cf8b7c6e23c52a5d4
Zhengyuan <sli695439@gmail.com>
2026-03-13 14:03:40 -0400

    LC-rotate-list [Runtime: 0 ms; Memory: 44.6 MB]

--- linked-list/rotate-list.java
+++ linked-list/rotate-list.java
@@ -10,26 +10,34 @@
  */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        if (head == null) return null;
-        //use a dummy head
-        //while k>0
-        //every time find the node's next is null, make it as the dummy next
-        ListNode dummy = new ListNode();
-        dummy.next = head;
-        ListNode prev = head;
+        //count the length of list n
+        //make the list to a circular list
+        //use k % n, if == 0 return the original head
+        //go through n - k% n
+        //split at that point, make prev.next = null, nextStart is head
+        //return nextStart
+        if (head == null || k == 0) return head;
+        int n = 1;
         ListNode curr = head;
-
-        while (k>0) {
-            if (curr.next == null) {
-                ListNode temp = dummy.next;
-                dummy.next = curr;
-                curr.next = temp;
-                prev.next = null;
-                k--;
-            }
-            prev = curr;
+        while(curr.next != null) {
+            n++;
             curr = curr.next;
         }
-        return dummy.next;
+        
+
+        k %= n;
+        if (k == 0) return head;
+        curr.next = head;
+        n = n - k;
+        
+        ListNode nextHead = head;
+        ListNode prev = head;
+        while (n > 0) {
+            prev = nextHead;
+            nextHead = nextHead.next;
+            n--;
+        }
+        prev.next = null;
+        return nextHead;
     }
 }
