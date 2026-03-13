15957f69c4b13fb025ae3642d2f229accd1eda03
Zhengyuan <sli695439@gmail.com>
2026-03-13 11:23:19 -0400

    LC-odd-even-linked-list [Runtime: N/A; Memory: N/A]

---
+++ linked-list/odd-even-linked-list.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        //using a odd head and a even head
+        //while not reach the last even node
+        //using a even head, which is the first even node
+        //link the odd pointer next to even head
+        //return head
+        ListNode odd = head;
+        ListNode even = odd.next;
+        ListNode evenHead = even;
+
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
+}
