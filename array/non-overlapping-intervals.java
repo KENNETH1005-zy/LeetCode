ae9319f40031b826ada15c979e6842891646de0c
Zhengyuan <sli695439@gmail.com>
2026-03-19 14:11:49 -0400

    LC-non-overlapping-intervals [Runtime: N/A; Memory: N/A]

--- array/non-overlapping-intervals.java
+++ array/non-overlapping-intervals.java
@@ -1,6 +1,5 @@
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
-        // Sort intervals by start time
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
 
         PriorityQueue<Integer> pq = new PriorityQueue<>(); // min-heap of end times
@@ -9,23 +8,19 @@ class Solution {
             int start = interval[0];
             int end = interval[1];
 
-            // If no overlap: current start >= last end
-            if (pq.isEmpty() || start >= pq.peek()) {
-                pq.add(end);
-            } else {
-                // Overlap: we remove this interval (don't add)
-                // But we can optionally replace the one with largest end time?
-                // Actually: we want to keep the one with smaller end time!
-                // So: if current end < top, replace it to improve future matches
+            // If there's an overlap with the earliest-ending interval we've kept
+            if (!pq.isEmpty() && start < pq.peek()) {
+                // Replace the one with the largest end time if current ends earlier
                 if (end < pq.peek()) {
-                    pq.poll();  // remove the larger end time
-                    pq.add(end); // add smaller end time
+                    pq.poll();
+                    pq.add(end);
                 }
-                // else: keep the old one, ignore current
+                // Otherwise, skip current
+            } else {
+                pq.add(end);
             }
         }
 
-        // Number of intervals removed = total - kept
         return intervals.length - pq.size();
     }
 }
