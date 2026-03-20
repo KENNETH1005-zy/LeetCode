93cc808bb97c4d5ab797a50588e5ae93b331e078
Zhengyuan <sli695439@gmail.com>
2026-03-20 13:42:42 -0400

    LC-house-robber [Runtime: N/A; Memory: N/A]

--- array/house-robber.java
+++ array/house-robber.java
@@ -1,23 +1,16 @@
 class Solution {
-    //a map to store max money for each index
-    //global nums
-    Map<Integer, Integer> map;
-    int[] nums;
     public int rob(int[] nums) {
-        map = new HashMap<>();
-        this.nums = nums;
-        //starting from last one to search
-        return dp(nums.length - 1);
-    }
-    //helper function to implement map
-    public int dp(int i) {
-        //there are two base case, first one and second one
-        if (i == 0) return nums[0];
-        if (i == 1) return Math.max(nums[1], nums[0]);
+        //bottum up
+        //create a dp array
+        //store the max along the way
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = nums[1];
 
-        if (!map.containsKey(i)) {
-            map.put(i, Math.max(dp(i - 1), dp(i - 2) + nums[i]));
+        for (int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
         }
-        return map.get(i);
+        //the last one is the max 
+        return dp[nums.length - 1];
     }
 }
