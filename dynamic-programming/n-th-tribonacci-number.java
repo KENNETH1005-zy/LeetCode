da0025d481893adf90d5cb0f037f91928e73b6e3
Zhengyuan <sli695439@gmail.com>
2026-03-20 14:14:20 -0400

    LC-n-th-tribonacci-number [Runtime: 0 ms; Memory: 41.8 MB]

--- dynamic-programming/n-th-tribonacci-number.java
+++ dynamic-programming/n-th-tribonacci-number.java
@@ -3,10 +3,13 @@ class Solution {
         //nth num means n - 1 in the array
         int[] dp = new int[n + 1];
         //base case
+        if (n <= 2) {
+            if (n <= 0) return 0;
+            else return 1;
+        }
         dp[0] = 0;
         dp[1] = 1;
         dp[2] = 1;
-        if (n <= 2) return dp[n];
 
         for (int i = 3; i<= n; i++) {
             dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
