cdf732a7dfd65739b37549288b09c22b2e086489
Zhengyuan <sli695439@gmail.com>
2026-03-20 13:38:27 -0400

    LC-house-robber [Runtime: 1 ms; Memory: 43.1 MB]

--- array/house-robber.java
+++ array/house-robber.java
@@ -13,7 +13,7 @@ class Solution {
     public int dp(int i) {
         //there are two base case, first one and second one
         if (i == 0) return nums[0];
-        if (i == 1) return nums[1];
+        if (i == 1) return Math.max(nums[1], nums[0]);
 
         if (!map.containsKey(i)) {
             map.put(i, Math.max(dp(i - 1), dp(i - 2) + nums[i]));
