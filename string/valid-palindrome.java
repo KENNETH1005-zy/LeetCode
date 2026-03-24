e775a5b76af9c2efe3ca821cbdbd2daf7b1acfa2
Zhengyuan <sli695439@gmail.com>
2026-03-24 18:31:50 -0400

    LC-valid-palindrome [Runtime: 2 ms; Memory: 44.4 MB]

--- string/valid-palindrome.java
+++ string/valid-palindrome.java
@@ -1,19 +1,20 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        //contvert all char to lower case
-        String t = "";
-        for (char c: s.toCharArray()) {
-            if (Character.isLetter(c)) {
-                t += Character.toLowerCase(c);
-            }else if (Character.isDigit(c)) {
-                t += c;
-            }
-        }
+        //two pointer,
+        //find the left pointer until is letter or digit
+        //also the right pointer
+        //if the valid left and right char are not the same return false
+        //return true
         int i = 0;
-        int j = t.length() - 1;
-
-        while (i <= j) {
-            if (t.charAt(i) != t.charAt(j)) {
+        int j = s.length()- 1;
+        while (i < j) {
+            while (i < j && !Character.isLetterOrDigit(s.charAt(i))) {
+                i++;
+            }
+            while (i < j && !Character.isLetterOrDigit(s.charAt(j))) {
+                j--;
+            }
+            if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) {
                 return false;
             }
             i++;
