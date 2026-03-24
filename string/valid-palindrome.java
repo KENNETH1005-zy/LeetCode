74b677da2ff65e71b066590586a82d52f6fc926c
Zhengyuan <sli695439@gmail.com>
2026-03-24 17:55:32 -0400

    LC-valid-palindrome [Runtime: N/A; Memory: N/A]

---
+++ string/valid-palindrome.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        //contvert all char to lower case
+        String t = "";
+        for (char c: s.toCharArray()) {
+            if (Character.isLetter(c)) {
+                t += Character.toLowerCase(c);
+            }
+        }
+        int i = 0;
+        int j = t.length() - 1;
+
+        while (i <= j) {
+            if (t.charAt(i) != t.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
