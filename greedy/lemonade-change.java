ff4a3f7fa762998cd1ef17a05880e5ce5d6ba49a
Zhengyuan <sli695439@gmail.com>
2026-03-26 15:42:55 -0400

    LC-lemonade-change [Runtime: 2 ms; Memory: 72.5 MB]

--- greedy/lemonade-change.java
+++ greedy/lemonade-change.java
@@ -1,33 +1,40 @@
 class Solution {
-    public boolean lemonadeChange(int[] bills) {
-        //if we get 5, not need to change
-        //if 10 find if we have 5
-        //if 20, find we have 1 5 and 1 10
-        //hashmap
-        Map<Integer, Integer> map = new HashMap<>();
 
-        for (int i = 0; i<bills.length; i++) {
-            int cash = bills[i];
-            if (cash == 5) {
-                map.put(cash, map.getOrDefault(cash, 0) + 1);
-            }else if (cash == 10) {
-                if (!map.containsKey(5) || map.get(5) == 0) {
+    public boolean lemonadeChange(int[] bills) {
+        // Count of $5 and $10 bills in hand
+        int fiveDollarBills = 0;
+        int tenDollarBills = 0;
+
+        // Iterate through each customer's bill
+        for (int customerBill : bills) {
+            if (customerBill == 5) {
+                // Just add it to our count
+                fiveDollarBills++;
+            } else if (customerBill == 10) {
+                // We need to give $5 change
+                if (fiveDollarBills > 0) {
+                    fiveDollarBills--;
+                    tenDollarBills++;
+                } else {
+                    // Can't provide change, return false
                     return false;
-                }else {
-                    map.put(5, map.get(5) - 1);
-                    map.put(cash, map.getOrDefault(cash, 0) + 1);
                 }
-            }else if (cash == 20) {
-                if (!map.containsKey(10) || !map.containsKey(5) ||
-                    map.get(10) == 0 || map.get(5) == 0) {
+            } else { // customerBill == 20
+                // We need to give $15 change
+                if (tenDollarBills > 0 && fiveDollarBills > 0) {
+                    // Give change as one $10 and one $5
+                    fiveDollarBills--;
+                    tenDollarBills--;
+                } else if (fiveDollarBills >= 3) {
+                    // Give change as three $5
+                    fiveDollarBills -= 3;
+                } else {
+                    // Can't provide change, return false
                     return false;
-                }else {
-                    map.put(10, map.get(10) - 1);
-                    map.put(5, map.get(5) - 1);
-                    map.put(cash, map.getOrDefault(cash, 0) + 1);
                 }
             }
         }
+        // If we've made it through all customers, return true
         return true;
     }
 }
