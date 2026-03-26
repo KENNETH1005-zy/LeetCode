49f386efb85fc9c16af89b906c44f9611ce46c50
Zhengyuan <sli695439@gmail.com>
2026-03-26 15:20:32 -0400

    LC-product-of-array-except-self [Runtime: 2 ms; Memory: 72.1 MB]

--- array/product-of-array-except-self.java
+++ array/product-of-array-except-self.java
@@ -1,22 +1,18 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        //run two times
-        //first dp array to store running sum to the left of i - 1
-        //dp[0] is 1 cause there is no left of 0th num
-        //then use a variable to record the running sum to the right of ith num
-        //time it to the dp[]array
-        //return the dp array
-        int[] dp = new int[nums.length];
-        dp[0] = 1;
+        //create prefix sum array
+        //start from the beginning, caculate mutiple sum before the current i
+        //start from the end, cacualte after
+        int[] prefix = new int[nums.length];
+        prefix[0] = 1;
         for (int i = 1; i<nums.length; i++) {
-            dp[i] = dp[i - 1] * nums[i - 1];
+            prefix[i] = nums[i - 1] * prefix[i - 1];
         }
-        int R = 1;
-
-        for (int i = nums.length - 1; i >= 0; i--) {
-            dp[i] = dp[i] * R;
-            R *= nums[i];
+        int r = 1;
+        for (int i = nums.length - 1; i>=0; i--) {
+            prefix[i] = prefix[i] * r;
+            r *= nums[i];
         }
-        return dp;
+        return prefix;
     }
 }
