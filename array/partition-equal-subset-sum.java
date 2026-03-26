7a337ea0e53201a466554601a4f25f60a822a514
Zhengyuan <sli695439@gmail.com>
2026-03-26 16:13:57 -0400

    LC-partition-equal-subset-sum [Runtime: 30 ms; Memory: 43.9 MB]

--- array/partition-equal-subset-sum.java
+++ array/partition-equal-subset-sum.java
@@ -1,21 +1,19 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-        //calculate the sum
-        //sum is to find the sum/2
-        //create an array with length of that target
-        //starting from the back, if the current target can be found, make 
-        //that true
-        int sum  = 0;
-        for (int num: nums) {
-            sum += num;
-        }
-        if (sum  % 2 != 0) return false;
-
+        //option1: add current num to the left
+        //opotion2: add current num to the right
+        //sum 
+        //target is half of the sum
+        //if mod is more 0, return false
+        //dp array define, each dp[i], means we have found the sum of i
+        int sum = Arrays.stream(nums).sum();
+        if (sum % 2 != 0) return false;
         int target = sum / 2;
         boolean[] dp = new boolean[target + 1];
-        dp[0] = true;
+        dp[0] = true;//sum of 0 can be found, which means nothing in it
+
         for (int num: nums) {
-            for (int j = target; j>= num; j--) {
+            for (int j = target; j>=num; j--) {
                 dp[j] |= dp[j - num];
             }
         }
