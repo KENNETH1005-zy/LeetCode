b859cb4f582ff17f267a75e28884902e13b1ec80
Zhengyuan <sli695439@gmail.com>
2026-03-27 20:56:01 -0400

    LC-meeting-rooms-ii [Runtime: 8 ms; Memory: 48.5 MB]

--- two-pointers/meeting-rooms-ii.java
+++ two-pointers/meeting-rooms-ii.java
@@ -1,13 +1,16 @@
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
-        if (intervals.length <= 1) return 1;
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-
+        //sort
+        //pq
+        //insert end time to pq
+        //if the new start time is more than peek(), poll() prev, add new ending time
         PriorityQueue<Integer> pq = new PriorityQueue<>();
+        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0], b[0]));
+
         pq.add(intervals[0][1]);
 
         for (int i = 1; i<intervals.length; i++) {
-            if (pq.peek() <= intervals[i][0]){
+            if (pq.peek() <= intervals[i][0]) {
                 pq.poll();
                 pq.add(intervals[i][1]);
             }else {
