3b98b6d25c2d00af0fc0fa78d4da12d4ece87371
Zhengyuan <sli695439@gmail.com>
2026-03-27 21:26:23 -0400

    LC-number-of-provinces [Runtime: 1 ms; Memory: 47.3 MB]

--- graph/number-of-provinces.java
+++ graph/number-of-provinces.java
@@ -1,35 +1,44 @@
-//use unionfind
+//use UnionFind class to connect the same province
 class UnionFind{
+    //an array to record parent of the element
+    //rank for each element
     int[] parent;
     int[] rank;
 
-    public UnionFind(int n){
+    public UnionFind(int n) {
         parent = new int[n];
+        rank = new int[n];
+
         for (int i = 0; i<n; i++) {
             parent[i] = i;
+            rank[i] = 1;
         }
-        rank = new int[n];
-    }
-
-    public int find(int num){
-        if (num != parent[num]) {
-            parent[num] = find(parent[num]);
-        }
-        return parent[num];
     }
 
     public void union(int i, int j) {
-        int iP = find(i), jP = find(j);
-        if (iP == jP) {
+        //find each parent
+        //compare the rank of their parents
+        //union them
+        int ip = find(i);
+        int jp = find(j);
+
+        if (ip == jp) {
             return;
-        }else if (rank[iP] > rank[jP]) {
-            parent[jP] = iP;
-        }else if (rank[iP] < rank[jP]){
-            parent[iP] = jP;
+        }else if (rank[ip] > rank[jp]) {
+            parent[jp] = ip;
+        }else if (rank[jp] > rank[ip]) {
+            parent[ip] = jp;
         }else {
-            parent[jP] = iP;
-            rank[iP]++;
+            parent[jp] = ip;
+            rank[ip]++;
         }
+    }
+
+    public int find(int i) {
+        if (parent[i] == i) {
+            return parent[i];
+        }
+        return find(parent[i]);
     }
 }
 class Solution {
