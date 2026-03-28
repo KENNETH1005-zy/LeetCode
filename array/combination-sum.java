dd82503060d3a32c316611b24c3216278a5669b1
Zhengyuan <sli695439@gmail.com>
2026-03-27 21:07:42 -0400

    LC-combination-sum [Runtime: 2 ms; Memory: 46.1 MB]

--- array/combination-sum.java
+++ array/combination-sum.java
@@ -1,31 +1,25 @@
 class Solution {
+    List<List<Integer>> result;
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        //set of list
-        //return list
-        Arrays.sort(candidates);
-        Set<List<Integer>> result = new HashSet<>();
-        dfs(0, 0,candidates, target, result, new ArrayList<>());
-        return new ArrayList<>(result);
+        //create the result
+        result = new ArrayList<>();
+        //dfs to find the sum is target
+        dfs(0, candidates, target, new ArrayList<>());
+        return result;
     }
-    //dfs to find the set of list that sum is target
-    //if total sum is more than target, backtrack to prev
-    //iterate in the cands, find the next one
-    public void dfs(int i, int sum, int[] candidates, int target, Set<List<Integer>> result, List<Integer> list){
-        //base base
-        if (sum == target) {
+    //helper function
+    public void dfs(int index, int[] candidates, int target, List<Integer> list) {
+        //base case
+        if (target == 0) {
             result.add(new ArrayList<>(list));
             return;
-        }else if (i >= candidates.length || sum > target) {
+        }else if (target < 0) {
             return;
         }
-
-        for (int j = i; j<candidates.length; j++) {
-            sum += candidates[j];
-            list.add(candidates[j]);
-
-            dfs(j, sum, candidates, target, result, list);
-
-            sum -= candidates[j];
+        for (int i = index; i<candidates.length; i++) {
+            list.add(candidates[i]);
+            dfs(i, candidates, target - candidates[i], list);
+            //backtracking
             list.remove(list.size() - 1);
         }
     }
