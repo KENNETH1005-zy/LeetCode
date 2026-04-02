f1c7d6c4b5d65298e99aeae66cf2010ce1a785e4
Zhengyuan <sli695439@gmail.com>
2026-04-02 17:55:52 -0400

    LC-reverse-linked-list [Runtime: 0 ms; Memory: 44.3 MB]

--- recursion/reverse-linked-list.java
+++ recursion/reverse-linked-list.java
@@ -10,19 +10,20 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        //set up a prev listnode
-        //iterate throught the list
-        //point to the prev one
-        //return the prev
-        ListNode prev = null;
-        ListNode curr = head;
-        while (curr != null) {
-            ListNode next = curr.next;
-            curr.next = prev;
-
-            prev = curr;
-            curr = next;
+        //Recursion:
+        //if the head is null, or the next is null, return the current head
+        if (head == null || head.next == null) {
+            return head;
         }
-        return prev;
+        //recursively find the last one in thelist
+        ListNode p = reverseList(head.next);
+        //now p is pointing to the last node of the original node
+        //make the current head's next, which is the next one in the prev list, 
+        //make its next to the head
+        head.next.next = head;
+        //and make current next is null, because remove the prev connection
+        head.next = null;
+        //back to last recursion
+        return p;
     }
 }
