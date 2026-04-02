68e293c95fc71db8f43313deaf0ce76a9eeac535
Zhengyuan <sli695439@gmail.com>
2026-04-02 16:01:02 -0400

    LC-binary-tree-maximum-path-sum [Runtime: N/A; Memory: N/A]

---
+++ tree/binary-tree-maximum-path-sum.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    //global sum to record the overall max sum
+    int[] result;
+    public int maxPathSum(TreeNode root) {
+        result= new int[1];
+        result[0] = Integer.MIN_VALUE;
+        dfs(root);
+        return result[0];
+    }
+
+    //find the max sum in each recursion
+    public int dfs(TreeNode node) {
+        //base case
+        if (node == null) {
+            return 0;
+        }
+
+        int left = dfs(node.left);
+        int right = dfs(node.right);
+
+        //include the current node
+        result[0] = Math.max(result[0], node.val + left + right);
+        //or exclude chose the bigger path between left and right
+        return Math.max(node.val + left, node.val + right);
+
+    }
+}
