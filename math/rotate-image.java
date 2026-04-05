9e8f48c6358c0b2c21ac290bf96cd1a3ee91b180
Zhengyuan <sli695439@gmail.com>
2026-04-05 18:22:08 -0400

    LC-rotate-image [Runtime: 0 ms; Memory: 43.8 MB]

---
+++ math/rotate-image.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        //starting from the first 0th row
+        //keep the first num, make the last in the row to the first num
+        //rotate it in clockwise
+        for (int i = 0; i <(n+1) / 2; i++) {
+            for (int j = 0; j < n / 2; j++) {
+               //set as left top as[i][j]
+               //right top [j][n - i - 1]
+               //right buttom [n - i - 1][n - 1 - j];
+               //left buttom [n - 1 - j][i]
+               
+               //set the left buttom, as the temp var
+               int temp = matrix[n - 1 - j][i];
+               matrix[n - 1 - j][i] = matrix[n - i - 1][n - 1- j];
+               matrix[n - i - 1][n - 1- j] = matrix[j][n - i - 1];
+               matrix[j][n - i - 1] = matrix[i][j];
+               matrix[i][j] = temp;
+            }
+        }
+    }
+}
