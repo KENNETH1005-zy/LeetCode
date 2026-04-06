be96ac925c9ea2714eaa1db53d7fc7e225e835f9
Zhengyuan <sli695439@gmail.com>
2026-04-06 15:11:02 -0400

    LC-can-place-flowers [Runtime: N/A; Memory: N/A]

---
+++ array/can-place-flowers.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        //if both front and end space is 0
+        //can be planted
+        //if not return true
+        for (int i = 0; i<flowerbed.length; i++) {
+            if (flowerbed[i] == 1) {
+                continue;
+            }
+            //now the num must be 0
+            if (i == 0) {
+                if (flowerbed[i + 1] == 0) {
+                    flowerbed[i] = 1;
+                    n--;
+                }
+            }else if (i == flowerbed.length - 1) {
+                if (flowerbed[i - 1] == 0) {
+                    flowerbed[i] = 1;
+                    n--;
+                }
+            }else {
+                if (flowerbed[i + 1] == 0 && flowerbed[i - 1] == 0) {
+                    flowerbed[i] = 1;
+                    n--;
+                }
+            }
+        }
+        return n <= 0;
+    }
+}
