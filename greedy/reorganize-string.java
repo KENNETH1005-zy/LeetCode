3cf34f0a684a879d5b18c864812d405d15c12b61
Zhengyuan <sli695439@gmail.com>
2026-04-07 11:51:22 -0400

    LC-reorganize-string [Runtime: 5 ms; Memory: 43.3 MB]

--- greedy/reorganize-string.java
+++ greedy/reorganize-string.java
@@ -1,49 +1,57 @@
+class Pair {
+    char c;
+    int count;
+    public Pair(char c, int count) {
+        this.c = c;
+        this.count = count;
+    }
+}
 class Solution {
     public String reorganizeString(String s) {
-        //use first most and second most freq char
-        //maxHeap to strore the char and the count, sorting on count
-        //if the sb is null or the first one poll out is not the same
-        //as the prev one, add it to the string
-        //if it is the same add it to the maxheap
-        //find the next most freq
-        //if maxHeap is null, at this time return ""
-        //if not add it to the sb
-        //return if maxHeap is null
-        //else return ""
+        //sb to build the result
+        //map to store each char's count
+        //pq to the char, and the count, max heap
+        //poll most frequent char from max heap,
+        //if the char is the same as prev char in result
+        //poll another one, if heap is empty return ""
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b) -> Integer.compare(b.count, a.count));
+        //add each char count into map
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c: s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        //store into pq
+        for (Map.Entry<Character, Integer> entry: map.entrySet()) {
+            char c = entry.getKey();
+            int count = entry.getValue();
 
-        //use an int[] to store the count for each
-        int[] counts = new int[26];
-        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a,b) -> Integer.compare(b[1], a[1]));
+            pq.offer(new Pair(c, count));
+        }
+        //construct the result string
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i<s.length(); i++) {
-            char c = s.charAt(i);
-            counts[c - 'a']++;
-        }
-        //for the counts that is more than 1
-        for (int i = 0; i<counts.length; i++) {
-            if (counts[i] > 0) {
-                maxHeap.add(new int[]{ i+ 'a', counts[i]});
+        while(!pq.isEmpty()) {
+            Pair p = pq.poll();
+            char c = p.c;
+            int count = p.count;
+            //if sb is null or not as before, append it , count--
+            if (sb.length() == 0 || sb.charAt(sb.length() - 1) != c) {
+                sb.append(c);
+                if (count - 1 > 0) {
+                pq.offer(new Pair(c, count - 1));
             }
-        }
-        //implement the sb, using the maxHeap
-        while (!maxHeap.isEmpty()) {
-            int[] first = maxHeap.poll();
-            if (sb.length() == 0 || sb.charAt(sb.length() - 1) != first[0]) {
-                sb.append((char) first[0]);
-                if (--first[1] >0) {
-                    maxHeap.add(first);
-                }
             }else {
-                //the same 
-                if (maxHeap.isEmpty()) return "";
-                int[] second = maxHeap.poll();
-
-                sb.append((char) second[0]);
-                if (--second[1] > 0) {
-                    maxHeap.add(second);
+                if (pq.isEmpty()) return "";
+                Pair next = pq.poll();
+                char nextCh = next.c;
+                int nextCount = next.count;
+                sb.append(nextCh);
+                if (nextCount - 1 > 0) {
+                    pq.offer(new Pair(nextCh, nextCount - 1));
                 }
-                maxHeap.add(first);
+                //did not use c
+                pq.offer(new Pair(c, count));
             }
+           
         }
         return sb.toString();
     }
