2529ae063e05c327bfc1a6830fb2cd9fb93d877c
Zhengyuan <sli695439@gmail.com>
2026-04-08 17:18:19 -0400

    LC-01-matrix [Runtime: 7 ms; Memory: 49.2 MB]

--- array/01-matrix.java
+++ array/01-matrix.java
@@ -1,54 +1,51 @@
 class Solution {
-    int m;
-    int n;
-    boolean[][] visited;
-    int[][] dirs = new int[][]{{0,1}, {1, 0}, {0,-1}, {-1, 0}};
     public int[][] updateMatrix(int[][] mat) {
-        //store the coordinate and the initial path length to q
-        //boolean[][] to make every 0 visited
-        //while q is not empty
-        //poll out the visited set
-        //find in valid neighbors, if meet unvisited, add the current path + 1
-        //and add it to the q
-        //return the implement matrix
-        m = mat.length;
-        n = mat[0].length;
-        int[][] matrix = new int[m][n];
-
-        visited = new boolean[m][n];
-        //{row, col, path}
-        Queue<int[]> q = new LinkedList<>();
-        //store every 0 and make them visited
+        //use dp [][]
+        //store the mat first
+        //first time, if it is a 1, find left and top and + 1
+        //second time , in reverse, find right and down num
+        int m = mat.length;
+        int n = mat[0].length;
+        int[][] dp = new int[m][n];
+        for (int i = 0; i< m; i++) {
+            for (int j = 0; j<n; j++) {
+                dp[i][j] = mat[i][j];
+            }
+        }
+        //first time
         for (int i = 0; i<m; i++) {
             for (int j = 0; j<n; j++) {
-                if (mat[i][j] == 0) {
-                    visited[i][j] = true;
-                    matrix[i][j] = 0;
-                    q.add(new int[]{i, j, 0});
-                }
-            }
-        }
-        
-        while (!q.isEmpty()) {
-            int[] temp = q.poll();
-            for (int[] dir: dirs) {
-                int newR = temp[0] + dir[0];
-                int newC = temp[1] + dir[1];
+                if (mat[i][j] == 0) continue;
 
-                if (!valid(newR, newC)) {
-                    continue;
+                //set the minstep as max
+                int minStep = m * n;
+                if (i > 0) {
+                    minStep = Math.min(minStep, dp[i - 1][j]);
                 }
-                q.add(new int[]{newR, newC, temp[2] + 1});
-                visited[newR][newC] = true;;
-                matrix[newR][newC] = temp[2] + 1;
+
+                if (j > 0) {
+                    minStep = Math.min(minStep, dp[i][j - 1]);
+                }
+                dp[i][j] = minStep + 1;
             }
         }
-        return matrix;
-    }
-    public boolean valid(int r, int c) {
-        if (r < 0 || r >= m || c < 0 || c >= n || visited[r][c]) {
-            return false;
+        //second time
+        for (int i = m - 1; i>= 0; i--) {
+            for (int j = n - 1; j>=0; j--) {
+                if (mat[i][j] == 0) continue;
+
+                int minStep = m * n;
+                if (i < m - 1) {
+                    minStep = Math.min(minStep, dp[i + 1][j]);
+                }
+
+                if (j < n - 1) {
+                    minStep = Math.min(minStep, dp[i][j + 1]);
+                }
+
+                dp[i][j] = Math.min(dp[i][j], minStep + 1);
+            }
         }
-        return true;
+        return dp;
     }
 }
