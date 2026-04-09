260cafa456f15e8225fc0cd047ad6f35b7d89bbd
Zhengyuan <sli695439@gmail.com>
2026-04-09 16:09:54 -0400

    LC-cheapest-flights-within-k-stops [Runtime: N/A; Memory: N/A]

---
+++ depth-first-search/cheapest-flights-within-k-stops.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        //use dest[] to store the min cost for each
+        //use k loop to find use under and equal to the current stop
+        //to implement the dest[]
+        //use temp[] to store the current dest
+        //rewrite the dest after loop
+        //return [dst]
+        int[] dests = new int[n];
+        Arrays.fill(dests, Integer.MAX_VALUE);
+        dests[src] = 0;
+        for (int i = 0; i<= k; i++) {
+            int[] temp = Arrays.copyOf(dests, n);
+            for (int[] flight: flights) {
+                int from = flight[0];
+                int to = flight[1];
+                int cost = flight[2];
+
+                if (dests[from] != Integer.MAX_VALUE ) {
+                    temp[to] = Math.min(dests[from] + cost, temp[to]);
+                }
+            }
+            dests = temp;
+        }
+        return dests[dst];
+    }
+}
