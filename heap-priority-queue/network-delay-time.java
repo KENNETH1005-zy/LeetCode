368f6a211e3de060e0918ffc3ce4b5110717e765
Zhengyuan <sli695439@gmail.com>
2026-04-09 15:32:24 -0400

    LC-network-delay-time [Runtime: 4 ms; Memory: 48.8 MB]

--- heap-priority-queue/network-delay-time.java
+++ heap-priority-queue/network-delay-time.java
@@ -1,49 +1,30 @@
 class Solution {
-    //{time, neighbor node}
-    Map<Integer, List<int[]>> map = new HashMap<>();
     public int networkDelayTime(int[][] times, int n, int k) {
-        //dp
-        //implement
-        //return max
-        int[] dp = new int[n + 1];
+        //bellman ford
+        int[] dp = new int[n+1];
         Arrays.fill(dp, Integer.MAX_VALUE);
         dp[k] = 0;
-        for (int[] time: times) {
-            map.putIfAbsent(time[0], new ArrayList<>());
-            //{time, neighbor node}
-            map.get(time[0]).add(new int[]{time[2], time[1]});
-        }
-        helper(dp, 0, k);
-        int result = Integer.MIN_VALUE;
-        for (int i = 1; i<= n; i++) {
-            result = Math.max(result, dp[i]);
-        }
-        return result == Integer.MAX_VALUE  ? -1 : result;
-    }
-    //helper function
-    //add initial
-    //while pq not null
-    //if new time is shorter, update
-    public void helper(int[] dp, int time, int source) {
-        //pq to store the earliest arrived node
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(a[0], b[0]));
-        pq.offer(new int[]{time, source});
-        while (!pq.isEmpty()) {
-            int[] temp = pq.poll();
-            int currNode = temp[1];
-            int currTime = temp[0];
-            if (currTime > dp[currNode]) continue;
-            if (!map.containsKey(currNode)) continue;
 
-            for (int[] neighbor: map.get(currNode)) {
-                int neighborNode = neighbor[1];
-                int nextTime = neighbor[0];
+        for (int i = 1; i<n; i++) {
+            boolean changed = false;
+            for (int[] time:times) {
+                int u = time[0];
+                int v = time[1];
+                int w = time[2];
 
-                if (dp[neighborNode] > currTime + nextTime) {
-                    dp[neighborNode] = currTime + nextTime;
-                    pq.offer(new int[]{dp[neighborNode], neighborNode});
+                if (dp[u] != Integer.MAX_VALUE && dp[u] + w < dp[v]) {
+                    dp[v] = dp[u] + w;
+                    changed = true;
                 }
             }
+            if (!changed) break;
         }
+
+        int result = Integer.MIN_VALUE;
+        for (int i = 1; i<=n; i++) {
+            if (dp[i] == Integer.MAX_VALUE) return -1;
+            result = Math.max(result, dp[i]);
+        }
+        return result;
     }
 }
