7c226662c753070760200de0bea6ecbd1b6e42b7
Zhengyuan <sli695439@gmail.com>
2026-04-13 12:57:09 -0400

    LC-jump-game-ii [Runtime: 1 ms; Memory: 47.2 MB]

--- dynamic-programming/jump-game-ii.java
+++ dynamic-programming/jump-game-ii.java
@@ -1,17 +1,18 @@
 class Solution {
     public int jump(int[] nums) {
-        //a var to store the maxEnd, which means the max index the prev num can jump
-        //a var to store the currEnd, which means the index, the current num can jump
-        int maxEnd = 0;
-        int currEnd = 0;
+        //record the curr jump and the minx jump
+        //if reach the end, break it
+        //and return the min jump
+        int curr = 0;
+        int maxSofar = 0;
         int steps = 0;
-        for (int i = 0; i< nums.length - 1; i++) {
-            currEnd = Math.max(currEnd, i + nums[i]);
 
-            //only if the current index is reach the prev max end
-            if (i == maxEnd) {
+        for (int i = 0; i< nums.length - 1; i++) {
+            curr = Math.max(curr, i + nums[i]);
+
+            if (i == maxSofar) {
                 steps++;
-                maxEnd = currEnd;
+                maxSofar = curr;
             }
         }
         return steps;
