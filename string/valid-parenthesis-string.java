a8be8701cd6d8d11d461bd825afa281ee2d320c1
Zhengyuan <sli695439@gmail.com>
2026-04-19 16:42:04 -0400

    LC-valid-parenthesis-string [Runtime: N/A; Memory: N/A]

---
+++ string/valid-parenthesis-string.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        //use a stack to store (
+        //every time find a ), pop a ( from the stack
+        //* is a wild card, use a new stack to store the wild card
+        //if current ) , and two stacks are both empty, return false
+        Stack <Character> left = new Stack<>();
+        Stack <Character> wild = new Stack<>();
+
+        for (char c: s.toCharArray()) {
+            if (c == '(') {
+                left.push('(');
+            }else if (c == '*') {
+                wild.push('*');
+            }else {
+                if (!left.isEmpty()) {
+                    left.pop();
+                }else if (!wild.isEmpty()) {
+                    wild.pop();
+                }else if (left.isEmpty() && wild.isEmpty()) {
+                    return false;
+                }
+            }
+        }
+        //if there is other ( still left, return false
+        if (!left.isEmpty()) {
+            return false;
+        }
+        return true;
+    }
+}
