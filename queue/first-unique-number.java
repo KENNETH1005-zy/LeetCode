84750b99251eefe1452be9b268bf839e8ebd3cc3
Zhengyuan <sli695439@gmail.com>
2026-04-26 18:13:57 -0400

    LC-first-unique-number [Runtime: 109 ms; Memory: 137.9 MB]

--- queue/first-unique-number.java
+++ queue/first-unique-number.java
@@ -1,47 +1,37 @@
 class FirstUnique {
-    //use a map to store the num and the indice
-    //find the unique one that occurs first
-    Map<Integer, PriorityQueue<Integer>> map;
-    List<Integer> list;
-    public FirstUnique(int[] nums) {
-        map = new HashMap<>();
-        list = new ArrayList<>();
-        for (int num: nums) {
-            list.add(num);
-        }
-        for (int i = 0; i<nums.length; i++) {
-            if (!map.containsKey(nums[i])) {
-                map.put(nums[i], new PriorityQueue<>());
-            }
-            map.get(nums[i]).add(i);
-        }
-    }
-    
-    public int showFirstUnique() {
-        //find the first unique one that occurs
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for (int num: map.keySet()) {
-            if (map.get(num).size() == 1) {
-                pq.add(map.get(num).peek());
-            }
-        }
-        return pq.isEmpty() ? -1 : list.get(pq.peek());
+  private Queue<Integer> queue = new ArrayDeque<>();
+  private Map<Integer, Boolean> isUnique = new HashMap<>();
 
+  public FirstUnique(int[] nums) {
+    for (int num : nums) {
+      // Notice that we're calling the "add" method of FirstUnique; not of the queue. 
+      this.add(num);
     }
-    
-    public void add(int value) {
-        list.add(value);
-        if (!map.containsKey(value)) {
-            map.put(value, new PriorityQueue<>());
-        }
-        map.get(value).add(list.size() - 1);
+  }
+
+  public int showFirstUnique() {
+    // We need to start by "cleaning" the queue of any non-uniques at the start.
+    // Note that we know that if a value is in the queue, then it is also in
+    // isUnique, as the implementation of add() guarantees this.
+    while (!queue.isEmpty() && !isUnique.get(queue.peek())) {
+      queue.remove();
     }
+    // Check if there is still a value left in the queue. There might be no uniques.
+    if (!queue.isEmpty()) {
+      return queue.peek(); // We don't want to actually *remove* the value.
+    }
+    return -1;
+  }
+
+  public void add(int value) {
+    // Case 1: We need to add the number to the queue and mark it as unique. 
+    if (!isUnique.containsKey(value)) {
+      isUnique.put(value, true);
+      queue.add(value);
+    // Case 2 and 3: We need to mark the number as no longer unique.
+    } else {
+      isUnique.put(value, false);
+    }
+  }
 }
-
-/**
- * Your FirstUnique object will be instantiated and called as such:
- * FirstUnique obj = new FirstUnique(nums);
- * int param_1 = obj.showFirstUnique();
- * obj.add(value);
- */
