ed2ffe256db8f2ae392cce801a2ada71ab89b003
Zhengyuan <sli695439@gmail.com>
2026-04-26 17:38:04 -0400

    LC-meeting-rooms-ii [Runtime: 8 ms; Memory: 48.4 MB]

--- two-pointers/meeting-rooms-ii.java
+++ two-pointers/meeting-rooms-ii.java
@@ -1,16 +1,18 @@
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
-        //sort
-        //pq
-        //insert end time to pq
-        //if the new start time is more than peek(), poll() prev, add new ending time
+        //find the intersections
+        //sort the intervals
+        //use pq to store the ending time for each intervals
+        //with the earliest finish time
+        //record the max size of pq
+        //return the size
+  
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0], b[0]));
-
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
         pq.add(intervals[0][1]);
 
         for (int i = 1; i<intervals.length; i++) {
-            if (pq.peek() <= intervals[i][0]) {
+            if (intervals[i][0] >= pq.peek()) {
                 pq.poll();
                 pq.add(intervals[i][1]);
             }else {
