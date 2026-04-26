9ce2bc87e4188ca4a528b931b3588ea5e02c2477
Zhengyuan <sli695439@gmail.com>
2026-04-26 18:54:34 -0400

    LC-merge-sorted-array [Runtime: N/A; Memory: N/A]

--- two-pointers/merge-sorted-array.java
+++ two-pointers/merge-sorted-array.java
@@ -1,16 +1,17 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        //three pointers
-        //p1 and p2
-        //another pointer from the last one in nums1, to 0th
-        //compare nums at p1 and p2, implement the bigger one, and decrease that pointer
-        //if p2 < 0, break, no more to manipulate
+        //the size of nums1 is m + n
+        //last one is m + n - 1
+        //iterate from the last one in nums2, and the last one in nums1
+        //implement the last one with the bigger one
+        //if implement the one in nums2, decrease the pointer in nums2
+        //else decrese the pointer in nums1
         int p1 = m - 1;
         int p2 = n - 1;
 
-        for (int p = m + n - 1; p >= 0; p--) {
+        for (int p = m + n - 1; p>=0; p--){
             if (p2 < 0) break;
-            if (p1 >= 0 && nums1[p1] >= nums2[p2]) {
+            if (p1 > 0 && nums1[p1] >= nums2[p2]) {
                 nums1[p] = nums1[p1];
                 p1--;
             }else {
