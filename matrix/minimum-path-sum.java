b95e83187861e236cc2667c043eaa8a63f71b63f
Zhengyuan <sli695439@gmail.com>
2026-04-28 13:13:57 -0400

    LC-minimum-path-sum [Runtime: 5 ms; Memory: 50.1 MB]

--- matrix/minimum-path-sum.java
+++ matrix/minimum-path-sum.java
@@ -1,26 +1,28 @@
 class Solution {
     public int minPathSum(int[][] grid) {
-        //use dp[][] to record least efforts to get to that black
-        //update it with the smallest along the way
-        //dp[i][j] = left / top min
-        int[][] dp = new int[grid.length][grid[0].length];
-        
-        for (int[] single: dp) {
-            Arrays.fill(single, Integer.MAX_VALUE);
-        }
-        dp[0][0] = grid[0][0];
+        //can only go down or right
+        //use dp[][] to record the min steps to get to that part
+        //start initialize with itself
+        //in other part, that has top and left,implement it with the smaller one and add it self
+        //once bottom right is implemented, return that
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
 
-        for (int i = 0; i< grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (i > 0 && j == 0) {
-                    dp[i][j] = dp[i - 1][j] + grid[i][j];
-                }else if (i == 0 && j > 0) {
-                    dp[i][j] = dp[i][j - 1] + grid[i][j];
-                }else if (i > 0 && j > 0) {
+        dp[0][0] = grid[0][0];
+        for (int i = 0; i<m; i++) {
+            for (int j = 0; j<n; j++) {
+                if (i > 0 && j > 0) {
                     dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+                }else if (i > 0 && j == 0) {
+                    dp[i][j] = dp[i - 1][j] + grid[i][j];
+                }else if (i == 0 && j> 0) {
+                    dp[i][j] = dp[i][j - 1] + grid[i][j];
                 }
+
+                if (i == m - 1 && j == n - 1) break;
             }
         }
-        return dp[grid.length - 1][grid[0].length - 1];
+        return dp[m - 1][n - 1];
     }
 }
