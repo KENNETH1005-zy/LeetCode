ce90c24c663fb6a645a8c2c2a4d70f2f2c228f59
Zhengyuan <sli695439@gmail.com>
2026-05-05 02:58:18 -0400

    LC-subarray-sum-equals-k [Runtime: 24 ms; Memory: 48.8 MB]

--- array/subarray-sum-equals-k.java
+++ array/subarray-sum-equals-k.java
@@ -1,19 +1,17 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        //using the hashmap to record the count of sum
-        Map<Integer, Integer> map = new HashMap<>();
-        //this means sum 0 occurs once
-        map.put(0, 1);
+        //use map tp store the count of the sum
+        //add the count if meets the multiple of the k
+        //return count
         int count = 0;
         int sum = 0;
-
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
         for (int num: nums) {
             sum += num;
-
             if (map.containsKey(sum - k)) {
                 count += map.get(sum - k);
             }
-
             map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
         return count;
