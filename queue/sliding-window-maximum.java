667040adbf38ec810fd107ac0e8a675b0112eab2
Zhengyuan <sli695439@gmail.com>
2026-05-05 05:03:46 -0400

    LC-sliding-window-maximum [Runtime: 43 ms; Memory: 150.5 MB]

--- queue/sliding-window-maximum.java
+++ queue/sliding-window-maximum.java
@@ -1,28 +1,35 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        if (nums == null || nums.length == 0) return new int[0];
-        
-        // 1. 修正结果数组长度：n - k + 1
-        int n = nums.length;
-        int[] result = new int[n - k + 1];
+        //list to store the result
+        //dq to store the max in a window
+        //add first for the max
+        //if first one is out of the bound, poll that out
+        //in every iteration, if the last one in the queue is smaller than the num
+        //poll that
 
-        // 2. 优先队列存储 [值, 下标]，值大的在堆顶
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[0] - a[0]);
+        List<Integer> result = new ArrayList<>();
+        Deque<Integer> dq = new LinkedList<>();
 
-        for (int i = 0; i < n; i++) {
-            // 将当前元素加入堆
-            pq.add(new int[]{nums[i], i});
-            
-            // 当索引达到 k-1 时，窗口正式形成
-            if (i >= k - 1) {
-                // 3. 核心：如果堆顶元素的下标已经不在当前窗口内 (<= i - k)，就弹出
-                while (pq.peek()[1] <= i - k) {
-                    pq.poll();
-                }
-                // 4. 当前堆顶就是窗口最大值
-                result[i - k + 1] = pq.peek()[0];
+        //for the first k nums
+        for (int i = 0; i<k; i++) {
+            while (!dq.isEmpty() && nums[dq.peekLast()] <= nums[i]) {
+                dq.pollLast();
             }
+            dq.addLast(i);
         }
-        return result;
+        result.add(nums[dq.peekFirst()]);
+        //first one is always the biggest one
+        for (int i = k; i<nums.length; i++) {
+            if (i - k == dq.peekFirst()) {
+                dq.pollFirst();
+            }
+
+            while (!dq.isEmpty() && nums[i] >= nums[dq.peekLast()]) {
+                dq.pollLast();
+            }
+            dq.offerLast(i);
+            result.add(nums[dq.peekFirst()]);
+        }
+        return result.stream().mapToInt(Integer :: intValue).toArray();
     }
 }
