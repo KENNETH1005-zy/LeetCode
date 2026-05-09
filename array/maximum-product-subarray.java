fabe929dede3e1e4f14162c80329d1845d5b7951
Zhengyuan <sli695439@gmail.com>
2026-05-09 10:30:47 -0400

    LC-maximum-product-subarray [Runtime: 2 ms; Memory: 47.5 MB]

--- array/maximum-product-subarray.java
+++ array/maximum-product-subarray.java
@@ -1,12 +1,18 @@
 class Solution {
     public int maxProduct(int[] nums) {
         //current sum and the total sum
-        int current = nums[0];
-        int total = nums[0];
+        //sum count be bigger
+     
+        //use the biggest and the smallest
+        int smallest = nums[0];
+        int biggest = nums[0];
+        int result = biggest;
         for (int i = 1; i<nums.length; i++) {
-            current = Math.max(nums[i], current * nums[i]);
-            total = Math.max(total, current);
+            int temp = Math.max(nums[i], Math.max(nums[i] * biggest, nums[i] * smallest));
+            smallest = Math.min(nums[i], Math.min(nums[i] * biggest, nums[i] * smallest));
+            biggest = temp;
+            result = Math.max(biggest, result);
         }
-        return total;
+        return result;
     }
 }
