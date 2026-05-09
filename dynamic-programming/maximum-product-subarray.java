ecefefc6720f5105fcaa1ca6c4518ccab12e835c
Zhengyuan <sli695439@gmail.com>
2026-05-09 10:21:24 -0400

    LC-maximum-product-subarray [Runtime: N/A; Memory: N/A]

--- dynamic-programming/maximum-product-subarray.java
+++ dynamic-programming/maximum-product-subarray.java
@@ -1,21 +1,12 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        // - * - could be positive
-        //so need to record smallest num
-        //and the biggest num as well
-        //global var to store biggest
-        int currMin = nums[0];
-        int currMax = nums[0];
-        int biggest = currMax;
-        for (int i = 1; i< nums.length; i++) {
-            int curr = nums[i];
-            
-            //temp max
-            int temp_max = Math.max(curr, Math.max(curr * currMin, curr * currMax));
-            currMin = Math.min(curr, Math.min(curr * currMin, curr * currMax));
-            currMax = temp_max;
-            biggest = Math.max(biggest, currMax);
+        //current sum and the total sum
+        int current = nums[0];
+        int total = nums[0];
+        for (int i = 1; i<nums.length; i++) {
+            current = Math.max(nums[i], current * nums[i]);
+            total = Math.max(total, current);
         }
-        return biggest;
+        return total;
     }
 }
