90df1d051ff89b1ca890de46a40c117842d54fe8
Zhengyuan <sli695439@gmail.com>
2026-05-11 17:46:41 -0400

    LC-coin-change [Runtime: 16 ms; Memory: 45.9 MB]

--- breadth-first-search/coin-change.java
+++ breadth-first-search/coin-change.java
@@ -1,20 +1,23 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        //use array to store the min numbers to make up that amount money
-        //for ex: [amount] means min count to make up amount
+        //return the fewest
+        //create an array that record the min change
+        //from 0 to that amount
+        //dp[0] is 0
+        //implement the coins index, in the dp[]
+        //return dp[amount]
+        //only implement the array if the current i is bigger or equal to the coin value
         int[] dp = new int[amount + 1];
-        //fill the dp with min value
-        Arrays.fill(dp, amount +1);
+        Arrays.fill(dp, amount + 1);
         dp[0] = 0;
-        for (int i = 1; i<= amount; i++) {
-            //iterate to find the fittable coint
-            for(int j = 0; j< coins.length; j++) {
-                //only update the dp if the coin is smaller than current amount
-                if (coins[j] <= i) {
+
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (i >= coins[j]) {
                     dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
                 }
             }
         }
-        return dp[amount] > amount ? -1: dp[amount];
+        return dp[amount] > amount ? -1 : dp[amount];
     }
 }
