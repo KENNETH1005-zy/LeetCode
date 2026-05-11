e1e74bbc7c4da3738c93f55d9b0c776f7ee8990c
Zhengyuan <sli695439@gmail.com>
2026-05-11 17:32:18 -0400

    LC-longest-increasing-subsequence [Runtime: 6 ms; Memory: 46.2 MB]

--- dynamic-programming/longest-increasing-subsequence.java
+++ dynamic-programming/longest-increasing-subsequence.java
@@ -1,25 +1,39 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        //int[]dp to define the length of longest subsequence before this i
-        //starting with all 1, because the num it self can be a subsequence
-        //for i = 1 , impelement the dp array
-        //for j = 0 j < i, to iterate the nums before current i
-        //update dp[i] with dp[j] + 1(add itself), and dp[i]
-        //iterate the dp array to update the longest
-        //return longest
+        //use list to store the increasing subsequence
+        //if the current one is bigger just append to the list
+        //else if smaller, use binary search to find the first one index that
+        //if euqual or bigger than the current num
+        //and set at that index the current num
+        List<Integer> list = new ArrayList<>();
+        list.add(nums[0]);
 
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp, 1);
-        for (int i = 1; i< nums.length; i++) {
-            for (int j = 0; j<i; j++) {
-
-                if (nums[i] > nums[j]) dp[i] = Math.max(dp[i], dp[j] + 1);
+        for (int i = 1; i<nums.length; i++) {
+            if (nums[i] > list.get(list.size() - 1)) {
+                list.add(nums[i]);
+            }else {
+                int j = bs(nums[i], list);
+                list.set(j, nums[i]);
             }
         }
-        int result = 0;
-        for (int num: dp) {
-            result = Math.max(result, num);
-        }
-        return result;
+        return list.size();
+    }
+    public int bs(int num, List<Integer> list) {
+        int l = 0;
+        int r = list.size() - 1;
+        while (l < r){
+            int mid = l + (r - l) / 2;
+
+            if (list.get(mid) == num) {
+                return mid;
+            }
+
+            if (list.get(mid) > num) {
+                r = mid;
+            }else {
+                l = mid + 1;
+            }
+        } 
+        return l;
     }
 }
