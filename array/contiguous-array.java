86f78f83b300a99e0385f7f3e05122bf34032f6b
Zhengyuan <sli695439@gmail.com>
2026-05-11 12:04:18 -0400

    LC-contiguous-array [Runtime: N/A; Memory: N/A]

--- array/contiguous-array.java
+++ array/contiguous-array.java
@@ -1,19 +1,21 @@
 class Solution {
     public int findMaxLength(int[] nums) {
-        //map to find the last occur of count
-        //if meet 1, add 1
-        //if 0, -1
-        //if find the count that has before
+        //find the longest array length for equal count of 1 and 0
+        //if meet with 1, add 1, if 0, minus 1
+        //every time find the same count, find last one and update the length
+        //map to store the index
         Map<Integer, Integer> map = new HashMap<>();
-        map.put(0, -1);
-        int count = 0;
+        map.put(0, - 1);
+        int sum = 0;
         int result = 0;
-        for (int i = 0; i<nums.length; i++) {
-            count += nums[i] == 1 ? 1 : -1;
-            if (map.containsKey(count)) {
-                result = Math.max(result, i - map.get(count));
+        for (int i = 0; i < nums.length; i++) {
+            int current = nums[i] == 1 ? 1 : -1;
+            sum += current;
+
+            if (!map.containsKey(sum)) {
+                map.put(sum, i);
             }else {
-                map.put(count, i);
+                result = Math.max(result, i - map.get(sum));
             }
         }
         return result;
