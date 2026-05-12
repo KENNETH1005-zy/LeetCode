4143c6c0ff615f26e4417b3a9b0e8d51b34efe37
Zhengyuan <sli695439@gmail.com>
2026-05-11 23:43:02 -0400

    LC-number-of-islands [Runtime: N/A; Memory: N/A]

--- breadth-first-search/number-of-islands.java
+++ breadth-first-search/number-of-islands.java
@@ -1,28 +1,30 @@
 class Solution {
+    char[][] grid;
     public int numIslands(char[][] grid) {
+        //if at least one of four dirs is connected, it is the same island
+        //dfs to implement the seen array
+        //if find a new island, add 1 to the reslt
+        this.grid = grid;
+        int result = 0;
         if (grid == null || grid.length == 0) return 0;
-        //find in four dirs, if there is a 1, consider it as a land
-        //make it as -1, and find a new land
-        int islands = 0;
         for (int i = 0; i<grid.length; i++) {
-            for (int j = 0; j<grid[0].length; j++) {
+            for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] == '1') {
-                    islands += 1;
-                    dfs(grid, i, j);
+                    dfs(i, j);
+                    result += 1;
                 }
             }
         }
-        return islands;
+        return result;
     }
-    public void dfs(char[][] grid, int r, int c) {
-        //base case
-        if (r < 0 || r >= grid.length || c < 0 || c >= grid[0].length || grid[r][c] == '0') {
+    public void dfs(int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0') {
             return;
         }
-        grid[r][c] = '0';
-        dfs(grid, r + 1, c);
-        dfs(grid, r - 1 , c);
-        dfs(grid, r, c + 1);
-        dfs(grid, r, c - 1);
+        grid[i][j] = '0';
+        dfs(i + 1, j);
+        dfs(i, j + 1);
+        dfs(i - 1, j);
+        dfs(i, j - 1);
     }
 }
