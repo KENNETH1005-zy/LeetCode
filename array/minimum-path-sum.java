a6e426002e5916ac81a95a0d4e10ab2f9d7845dd
Zhengyuan <sli695439@gmail.com>
2026-05-21 14:19:41 -0400

    LC-minimum-path-sum [Runtime: 101 ms; Memory: 50.5 MB]

--- array/minimum-path-sum.java
+++ array/minimum-path-sum.java
@@ -1,28 +1,32 @@
 class Solution {
+    int[][] dp;
+
     public int minPathSum(int[][] grid) {
-        //can only go down or right
-        //use dp[][] to record the min steps to get to that part
-        //start initialize with itself
-        //in other part, that has top and left,implement it with the smaller one and add it self
-        //once bottom right is implemented, return that
-        int m = grid.length;
-        int n = grid[0].length;
-        int[][] dp = new int[m][n];
-
-        dp[0][0] = grid[0][0];
-        for (int i = 0; i<m; i++) {
-            for (int j = 0; j<n; j++) {
-                if (i > 0 && j > 0) {
-                    dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
-                }else if (i > 0 && j == 0) {
-                    dp[i][j] = dp[i - 1][j] + grid[i][j];
-                }else if (i == 0 && j> 0) {
-                    dp[i][j] = dp[i][j - 1] + grid[i][j];
-                }
-
-                if (i == m - 1 && j == n - 1) break;
+       //fron top left to bottom right
+       //return the min sum
+       //use an next array to store the min path sum to that cell
+       //everytime reach the bottom right update the result
+       //return the min one
+       dp = new int[grid.length][grid[0].length];
+       dp[0][0] = grid[0][0];
+       
+       for (int i = 0; i<grid.length; i++) {
+           for (int j = 0; j <grid[0].length; j++) {
+            if (i == 0 && j > 0) {
+                //if in the 0th row and more than 0th col
+                dp[i][j] = dp[i][j - 1] + grid[i][j];
+            }else if (i > 0 && j == 0) {
+                //if in the 0th col
+                dp[i][j] = dp[i - 1][j] + grid[i][j];
+            }else if (i > 0 && j > 0) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
             }
-        }
-        return dp[m - 1][n - 1];
+            if (i == grid.length - 1 && j == grid[0].length - 1) {
+                break;
+            }
+           }
+       }
+       return dp[grid.length - 1][grid[0].length - 1];
     }
+    
 }
