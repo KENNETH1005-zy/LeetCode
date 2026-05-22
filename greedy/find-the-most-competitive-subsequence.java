e37b99bb85d692bde8210e75db6b60a0778895a4
Zhengyuan <sli695439@gmail.com>
2026-05-22 12:22:37 -0400

    LC-find-the-most-competitive-subsequence [Runtime: N/A; Memory: N/A]

--- greedy/find-the-most-competitive-subsequence.java
+++ greedy/find-the-most-competitive-subsequence.java
@@ -1,12 +1,19 @@
 class Solution {
     public int[] mostCompetitive(int[] nums, int k) {
-        //use double ended deque, to store the smallest int[]
-        Deque<Integer> dq = new LinkedList<>();
+        //size is k
+        //find the subsequence that every index in this sequence is smaller
+        //than any other subsequence
+        //the smaller index num should be as small as possible
+
+        //record the limit of throwing away in the nums
         int additional = nums.length - k;
+        Deque<Integer> dq = new LinkedList<>();
         for (int i = 0; i<nums.length; i++) {
+            //if last one is more than the new num
+            //always make sure the num in in the last is smaller than the new num
             while (!dq.isEmpty() && dq.peekLast() > nums[i] && additional > 0) {
-                dq.pollLast();
                 additional--;
+                dq.pollLast();
             }
             dq.addLast(nums[i]);
         }
