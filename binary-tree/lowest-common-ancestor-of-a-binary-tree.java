4c72749f154fa25dc96b3b910ba0f6083fbe2fb6
Zhengyuan <sli695439@gmail.com>
2026-07-30 18:34:55 -0500

    LC-lowest-common-ancestor-of-a-binary-tree [Runtime: 15 ms; Memory: 69.7 MB]

--- binary-tree/lowest-common-ancestor-of-a-binary-tree.java
+++ binary-tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -8,28 +8,27 @@
  * }
  */
 class Solution {
-    //global result node
     TreeNode result;
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null) return null;
+        //if a node is parent of another node, just return the node
+        //starting from root find p and q
+        //record the current node as parent and find another one
+        result = new TreeNode();
         dfs(root, p, q);
         return result;
     }
-    //helper function to dectect if hit p or q, 
-    //in left and right and mid node, only contains one, return true, means find it
-    public boolean dfs(TreeNode root, TreeNode p, TreeNode q) {
+    //helper function to dectect whether p or q is found
+    public boolean dfs(TreeNode node, TreeNode p, TreeNode q) {
         //base case
-        if (root == null) {
+        if (node == null) {
             return false;
         }
-        int left = dfs(root.left, p, q) ? 1 : 0;
-        int right = dfs(root.right, p, q) ? 1 : 0;
-        int mid = (root == p || root == q) ? 1 : 0;
-
+        int left = dfs(node.left, p, q) ? 1: 0;
+        int right = dfs(node.right, p, q) ? 1 : 0;
+        int mid = (node == p) || node == q ? 1: 0;
         if ((left + right + mid) >= 2) {
-            result = root;
+            result = node;
         }
         return left + right + mid > 0;
     }
-
 }
