6a18f68000bc96a3d315ab25420ad0700335caff
Zhengyuan <sli695439@gmail.com>
2026-07-30 22:26:25 -0500

    LC-validate-binary-search-tree [Runtime: 0 ms; Memory: 45.3 MB]

--- binary-search-tree/validate-binary-search-tree.java
+++ binary-search-tree/validate-binary-search-tree.java
@@ -14,27 +14,24 @@
  * }
  */
 class Solution {
+    //record the smallest node sofar
+    Integer smallest;
     public boolean isValidBST(TreeNode root) {
-        //cause java int is pass by value, we need a variable that can pass in reference,
-        //which can retrive pass in reference
-        Integer[] curr = new Integer[1];
-        curr[0] = null;
-        return dfs(root, curr);
+        smallest = null;
+        return dfs(root);
     }
-    public boolean dfs(TreeNode node, Integer[] curr) {
+    public boolean dfs(TreeNode node) {
         //base case
-        if (node == null) return true;
-
-        boolean left = dfs(node.left, curr);
-        //if prev node is smaller than current, means this is a valid node
-        //update the curr node
-        if (curr[0] == null || curr[0] < node.val) {
-            curr[0] = node.val;
+        if (node == null) {
+            return true;
+        }
+        boolean left = dfs(node.left);
+        if (smallest == null || smallest < node.val) {
+            smallest = node.val;
         }else {
             return false;
         }
-        boolean right = dfs(node.right, curr);
-
+        boolean right = dfs(node.right);
         return left && right;
     }
 }
